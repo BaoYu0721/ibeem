040585c0e399bfd27db45e5ffe0277b406fcffae
BaoYu0721 <fairytale12345@163.com>
2019-01-08 12:09:27 +0800

    建筑维度上的分析
    建筑维度上的分析

--- app/service/survey/analyze.js
+++ app/service/survey/analyze.js
@@ -325,7 +325,184 @@ class AnalyzeService extends Service {
                 }
             }
             else if (type == "building") {
-                // const list = 
+                // List<BuildingSurvey> list = buildingSurveyService.getListBySurveyAndRelation(surveyID,relation,objectID);
+                var list = null;
+                if (relation == 0) {
+                    list = await this.app.mysql.select('building_survey', {where: {survey_id: surveyID}});
+                }
+                else if (relation == 1) {
+                    list = await this.app.mysql.query('select * from building_survey where survey_id=? and building_id in (select id from building where project_id=?)',
+                        [surveyID, objectID]);
+                }
+                
+                if (list != null && list.length != 0) {
+                    result['isNotRelated'] = 0;
+                    const yquestion = await this.app.mysql.get('question', {id: yid});
+                    if (yquestion.type == 1) {
+                        // 单选题
+                        var yquestion_json = JSON.parse(yquestion.setting);
+                        var yarray = yquestion_json.items;
+                        for (var i = 0; i < list.length; i++) {
+                            var building_survey = list[i];
+                            var map = {};
+                            const building = await this.app.mysql.get('building', {id: building_survey.building_id});
+                            const building_name = building.name;
+                            const building_id = building_survey.building_id;
+
+                            // answerDetailDao.getListByQuestion(yid,surveyID,buildingId,2,startTime,endTime)
+                            const yanswer_detail_list = await this.service.survey.answer.answerDetailGetListByQuestion(yid, surveyID, building_id, 2, beginTime, endTime);
+                            if (yanswer_detail_list == -1) {
+                                return -1;
+                            }
+
+                            if (yanswer_detail_list.length != 0) {
+                                is_no_data = 0;
+                                for (var j = 0; j < yarray.length; j++) {
+                                    map[yarray[j].text] = 0;
+                                }
+                                for (var j = 0; j < yarray.length; j++) {
+                                    var id = yarray[j].id;
+                                    for (var k = 0; k < yanswer_detail_list.length; k++) {
+                                        if (yanswer_detail_list[k].isanswered == 1) {
+                                            const answer_json = JSON.parse(yanswer_detail_list[k].reply_content);
+                                            if (id == answer_json.id) {
+                                                map[yarray[j].text] = map[yarray[j].text] + 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            result[building_name] = map;
+                        }
+                        result['isNoData'] = is_no_data;
+                    }
+                    else if (yquestion.type == 2) {
+                        // 多选题
+                        var yquestion_json = JSON.parse(yquestion.setting);
+                        var yarray = yquestion_json.items;
+                        for (var i = 0; i < list.length; i++) {
+                            var building_survey = list[i];
+                            var map = {};
+                            const building = await this.app.mysql.get('building', {id: building_survey.building_id});
+                            const building_name = building.name;
+                            const building_id = building_survey.building_id;
+
+                            // answerDetailDao.getListByQuestion(yid,surveyID,buildingId,2,startTime,endTime)
+                            const yanswer_detail_list = await this.service.survey.answer.answerDetailGetListByQuestion(yid, surveyID, building_id, 2, beginTime, endTime);
+                            if (yanswer_detail_list == -1) {
+                                return -1;
+                            }
+
+                            if (yanswer_detail_list.length != 0) {
+                                is_no_data = 0;
+                                for (var j = 0; j < yarray.length; j++) {
+                                    map[yarray[j].text] = 0;
+                                }
+                                for (var j = 0; j < yarray.length; j++) {
+                                    var id = yarray[j].id;
+                                    for (var k = 0; k < yanswer_detail_list.length; k++) {
+                                        if (yanswer_detail_list[k].isanswered == 1) {
+                                            const yanswer_json = JSON.parse(yanswer_detail_list[k].reply_content);
+                                            const yanswers = yanswer_json.answers;
+                                            for (var l = 0; l < yanswers.length; l++) {
+                                                if (id == yanswers[l].id) {
+                                                    map[yarray[j].text] = map[yarray[j].text] + 1;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            result[building_name] = map;
+                        }
+                        result['isNoData'] = is_no_data;
+                    }
+                    else if (yquestion.type == 0) {
+                        // 填空题
+                        var result_list = [];
+                        for (var i = 0; i < list.length; i++) {
+                            var building_survey = list[i];
+                            var text_list = [];
+                            var map = {};
+                            const building = await this.app.mysql.get('building', {id: building_survey.building_id});
+                            const building_name = building.name;
+                            const building_id = building_survey.building_id;
+                            map['id'] = building_id;
+                            // answerDetailDao.getListByQuestion(yid,surveyID,buildingId,2,startTime,endTime)
+                            const yanswer_detail_list = await this.service.survey.answer.answerDetailGetListByQuestion(yid, surveyID, building_id, 2, beginTime, endTime);
+                            if (yanswer_detail_list == -1) {
+                                return -1;
+                            }
+
+                            if (yanswer_detail_list.length != 0) {
+                                is_no_data = 0;
+                                for (var j = 0; j < yanswer_detail_list.length; j++) {
+                                    if (yanswer_detail_list[j].isanswered == 1) {
+                                        var m = {};
+                                        const reply_content = JSON.parse(yanswer_detail_list[j].reply_content);
+                                        const answer = reply_content.answer;
+                                        const tmp_date = new Date(yanswer_detail_list[j].created_on);
+                                        const date_str = tmp_date.getFullYear() + '-' + (tmp_date.getMonth()+1) + '-' + tmp_date.getDate() + ' ' + tmp_date.getHours() + ':' + tmp_date.getMinutes() + ':' + tmp_date.getSeconds();
+                                        m['time'] = date_str;
+                                        m['answer'] = answer;
+                                        text_list.push(m);
+                                    }
+                                }
+                            }
+
+                            map['textlist'] = text_list;
+                            result_list.push(map);
+                        }
+                        result['isNoData'] = is_no_data;
+                        result['analysis'] = result_list;
+                    }
+                    else if (yquestion.type == 3) {
+                        // 量表题
+                        // here
+                        const yquestion_json = JSON.parse(yquestion.setting);
+                        const yarray = yquestion_json.y_axis;
+                        const xarray = yquestion_json.x_axis;
+                        for (var i = 0; i < yarray.length; i++) {
+                            var m = {};
+                            const id = yarray[i].id;
+                            for (var j = 0; j < list.length; j++) {
+                                const project_survey = list[j];
+                                const project = await this.app.mysql.get('project', {id: project_survey.project_id});
+                                const project_name = project.name;
+                                const project_id = project_survey.project_id;
+                                // answerDetailDao.getListByQuestion(yid,surveyID,projectId,1,startTime,endTime)
+                                const yanswer_detail_list = await this.service.survey.answer.answerDetailGetListByQuestion(yid, surveyID, project_id, 1, beginTime, endTime);
+                                if (yanswer_detail_list == -1) {
+                                    return -1;
+                                }
+                                var zm = {};
+                                if (yanswer_detail_list.length != 0) {
+                                    is_no_data = 0;
+                                    for (var k = 0; k < xarray.length; k++) {
+                                        zm[xarray[k].tag] = 0;
+                                    }
+                                    for (var k = 0; k < yanswer_detail_list.length; k++) {
+                                        if (yanswer_detail_list[k].isanswered == 1) {
+                                            const yanswer_json = JSON.parse(yanswer_detail_list[k].reply_content);
+                                            const answers = yanswer_json.answers;
+                                            for (var l = 0; l < xarray.length; l++) {
+                                                const val = xarray[l].val;
+                                                for (var n = 0; n < answers.length; n++) {
+                                                    if (id == answers[n].id && val == answers[n].val) {
+                                                        zm[xarray[l].tag] = zm[xarray[l].tag] + 1;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    m[project_name] = zm;
+                                }
+                            }
+                            result[yarray[i].left] = m;
+                        }
+                        result['isNoData'] = is_no_data;
+                    }
+                }
             }
 
             return result;
@@ -334,8 +511,6 @@ class AnalyzeService extends Service {
             console.log('[service.survey.analyze.analysisSurvey]: error! --- ' + error);
             return -1;
         }
-
-        return -1;
     }
 }
 
