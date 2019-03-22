71c9432d7a515a81288b0b8c06819430d982af52
kangkanglinux <kangkanglinux@163.com>
2019-03-22 20:00:02 +0800

    Bug修复

--- app/schedule/check_device_status.js
+++ app/schedule/check_device_status.js
@@ -37,10 +37,14 @@ class CheckDeviceStatus extends Subscription{
                         };
                         const coclean_res2 = await ctx.service.utils.http.cocleanPost(this.app.config.deviceDataReqUrl.coclean.deviceIsOnline, param);
                         if(coclean_res2.result == 'success'){
-                            const coclean_status = {
-                                device_id:     device[0].id,
-                                online_status: coclean_res2.data
-                            };
+                            const coclean_status = new Object();
+                            if(coclean_res2.data == 1){
+                                coclean_status.online_status = 1;
+                            }
+                            else{
+                                coclean_status.online_status = 0;
+                            }
+                            coclean_status.device_id = device[0].id;
                             status.push(coclean_status);
                         }
                         const index = physical_ids.indexOf(coclean_res1.errorMsg.data[i]);
@@ -63,16 +67,19 @@ class CheckDeviceStatus extends Subscription{
             for(var key in ibeem){
                 const param = "q=" + ibeem[key].deviceid;
                 const ibeem_res = await ctx.service.utils.http.ibeemGet(this.app.config.deviceDataReqUrl.ibeem.getDeviceOnlineStatus, param);
-                if(!ibeem_res.status) continue;
-                const ibeem_status = {
-                    device_id:  ibeem[key].id,
-                    online_status: ibeem_res.status
-                };
+                const ibeem_status = new Object();
+                if(ibeem_res.status == 1){
+                    ibeem_status.online_status = 1;
+                }
+                else{
+                    ibeem_status.online_status = 0;
+                }
+                ibeem_status.device_id = ibeem[key].id;
                 status.push(ibeem_status);
             }
-
+            console.log(status);
             for(var key in status){
-                await this.app.mysql.update('device', {id: status[key].id, Online_status: status[key].online_status});
+                await this.app.mysql.update('device', {id: status[key].device_id, Online_status: status[key].online_status});
             }
         } catch (error) {
             console.log('CheckDeviceStatus Error' + error);
